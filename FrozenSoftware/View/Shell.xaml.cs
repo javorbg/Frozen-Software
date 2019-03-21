20b4b0415708c8cd09f2640cf4d980bb71f3b05b
javorbg <javorbg@gmail.com>
2019-03-21 22:08:44 +0200

    Add Shell, prism and Autofac DP

--- FrozenSoftware/View/Shell.xaml.cs
+++ FrozenSoftware/View/Shell.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FrozenSoftware.Models
+namespace FrozenSoftware
 {
     /// <summary>
-    /// Interaction logic for UserControl1.xaml
+    /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class Shell : Window
     {
-        public UserControl1()
+        public Shell()
         {
             InitializeComponent();
         }
