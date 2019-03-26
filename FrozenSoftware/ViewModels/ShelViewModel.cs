dcf448fef5e8d90315df114e7ee9b5414e6fe293
javorbg <javorbg@gmail.com>
2019-03-26 22:41:15 +0200

    Remove Usings

--- FrozenSoftware/ViewModels/ShelViewModel.cs
+++ FrozenSoftware/ViewModels/ShelViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FrozenSoftware.ViewModels
+﻿namespace FrozenSoftware.ViewModels
 {
     public class ShelViewModel
     {
