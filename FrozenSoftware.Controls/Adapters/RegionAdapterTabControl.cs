e086268f2f53839ee84e137203fbd8ead99d4077
javorbg <javorbg@gmail.com>
2019-03-27 20:22:10 +0200

    Add Controls project

---
+++ FrozenSoftware.Controls/Adapters/RegionAdapterTabControl.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Regions;
+using System.Windows.Controls;
+
+namespace FrozenSoftware.Controls.Adapters
+{
+    public class RegionAdapterTabControl : RegionAdapterBase<TabControl>
+    {
+        public RegionAdapterTabControl(IRegionBehaviorFactory regionBehaviorFactory)
+            : base(regionBehaviorFactory)
+        {
+        }
+        protected override void Adapt(IRegion region, TabControl regionTarget)
+        {
+        }
+
+        protected override IRegion CreateRegion()
+        {
+            return null;
+        }
+    }
+}
