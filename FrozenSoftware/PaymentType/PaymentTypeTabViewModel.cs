515757f704aca9cf02f5e9f1517c55079888d662
javorbg <javorbg@gmail.com>
2019-03-28 22:08:32 +0200

    Add ResourceDictionary and add Payment Type tab and view model

---
+++ FrozenSoftware/PaymentType/PaymentTypeTabViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FrozenSoftware.Controls;
+using FrozenSoftware.Models;
+using Prism.Regions;
+using System.Collections.ObjectModel;
+using Unity;
+
+
+namespace FrozenSoftware.PaymentType
+{
+    public class PaymentTypeTabViewModel : TabBaseViewModel
+    {
+        public PaymentTypeTabViewModel(IRegionManager regionManger, IUnityContainer unityContainer) : 
+            base(regionManger, unityContainer)
+        {
+        }
+    }
+}
