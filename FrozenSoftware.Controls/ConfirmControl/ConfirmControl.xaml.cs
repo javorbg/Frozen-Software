64df262d1320b222ba4cc08eedfdd10231fb37c3
javorbg <javorbg@gmail.com>
2019-04-24 00:21:45 +0300

    Add Confirm Cotnrol

---
+++ FrozenSoftware.Controls/ConfirmControl/ConfirmControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FrozenSoftware.Controls
+{
+    /// <summary>
+    /// Interaction logic for ConfirmControl.xaml
+    /// </summary>
+    public partial class ConfirmControl : UserControl
+    {
+        public ConfirmControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
